0a837aa5d620bf00068c349b17c77b5ec061382a
hoseinnadali <hoseinnadali74@gmail.com>
2020-03-17 19:04:25 +0330

    create a new file 'readdones.php' to display records that is done

---
+++ readdones.php
@@ -0,0 +1,19 @@
+<?php
+//this file will doing read the records from database that seted on done by isDone value from ocifetchstatement
+require_once "main.php";
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+  $readDonesCrud = new MyCrud();
+  $readDonesQuery = "SELECT title FROM $readDonesCrud->tableName WHERE isDone=1";
+  $readDonesConnection = $readDonesCrud->setConnection();
+  $readDonesCrud->read($readDonesConnection,$readDonesQuery);
+  exit();
+}
+echo "Sorry!!!There is missing request";
+
+
+
+
+
+
+ ?>
