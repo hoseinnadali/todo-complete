e4d88aa50ef0d01cf9bafbea71675840fa8aa477
hoseinnadali <hoseinnadali74@gmail.com>
2020-03-17 10:46:37 +0330

    debugging the main.php to run directly

--- main.php
+++ main.php
@@ -6,23 +6,30 @@ define("password","");
 define("dbName","tododb");
 define("tableName","todolist");
 
+
 class MyCrud{
+  public function __construct(){
+    $this->serverName = serverName;
+    $this->userName = userName;
+    $this->password = password;
+    $this->dbName = dbName;
+    $this->tableName = tableName;
+  }
   public function setConnection(){
     try {
-      $connection = new PDO("mysql : host = serverName,dbname = dbName",userName,password);
+      $connection = new PDO("mysql:host=".$this->serverName.";dbname=".$this->dbName,$this->userName,$this->password);
       $connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
       echo "Connected successfully!!!";
-
+      return $connection;
     } catch (PDOException $e) {
       echo "Connection failed: ".$e->getMessage();
     }
-    return $connection;
   }
   public function insert($insertConnection,$title,$description,$dueDate,$addDate,$editDate,$isDone){
     try {
       $insertConnection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-      $query = "INSERT INTO tableName (title,description,dueDate,addDate,editDate,isDone)
-      VALUES ($title,$description,$dueDate,$addDate,$editDate,$isDone)";
+      $query ="INSERT INTO $this->tableName (title,description,dueDate,addDate,editDate,isDone)
+      VALUES ('$title','$description','$dueDate','$addDate','$editDate','$isDone')";
       $insertConnection->exec($query);
       echo "New record created successfully";
       $insertConnection = null;
